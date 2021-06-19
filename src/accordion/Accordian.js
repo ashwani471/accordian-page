92f6218a072e54a28818d6e24ae0186a5c898b18
ashawni471 <ashwanikumar351999@gmail.com>
2021-06-19 02:45:02 -0700

    initial commit

---
+++ src/accordion/Accordian.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+import { questions } from "./api";
+// import MyAccordian from './MyAccordian';
+import MyAccordian from './MyAccordion';
+import './accordion.css';
+
+const Accordian = () => {
+    const [data, setData] = useState(questions);
+    return (
+        <>
+            <section className="main-div">
+                <h1>React Interview Questions </h1>
+        {
+                data.map((curElem) => {
+                    return <MyAccordian key={curElem.id} {...curElem} />
+                    //here the spread operator is use to pass the the properties of the questions api
+                })        
+        }
+                 </section>
+        </>
+    )
+}
+
+
+export default Accordian;
